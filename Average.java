ff32c92c13663d6dd3b2bc4873320f9468349ebf
YASHIKA MAHAWAR <honeymahawar18050309@gmail.com>
2026-04-06 11:37:23 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,11 @@
+// Question: Find average of 3 numbers
+// Output: 20
+
+public class Average {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        int avg = (a + b + c) / 3;
+        System.out.println(avg);
+    }
+}
+
